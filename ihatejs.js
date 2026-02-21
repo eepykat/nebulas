5353bf59da20525af7b7077eb477306b28f92a0d
Azalea ;3 <lockedinprogrammersocks@catgirl.email>
2026-02-21 14:38:50 +0200

    it doesnt work, i wasted my time ig

---
+++ ihatejs.js
@@ -0,0 +1,18 @@
+const elementsToAnimate = document.querySelectorAll('container1');
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    treshold: 0.1
+};
+const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('is-visible');
+            observer.unobserve(entry.target);
+        }
+    });
+};
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+elementsToAnimate.forEach(element => {
+    observer.observe(element);
+});
