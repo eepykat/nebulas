3def622e6ff16c37468bf412ad0d28883438f23b
Kat <kat@feline.email>
2026-02-25 07:56:02 -0800

    lots of changes, making sure site works on mobile and boxes are same size

--- ihatejs.js
+++ ihatejs.js
@@ -1,8 +1,10 @@
 const elementsToAnimate = document.querySelectorAll('#about-us');
+
+
 const observerOptions = {
     root: null,
     rootMargin: '0px',
-    treshold: 0.15
+    treshold: 0.1
 };
 const observerCallback = (entries, observer) => {
     entries.forEach(entry => {
@@ -17,15 +19,34 @@ elementsToAnimate.forEach(element => {
     observer.observe(element);
 });
 
+
+document.addEventListener('DOMContentLoaded', () => {
 const observer2 = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add('show');
+      console.log('JavaScript Works');
     }
   });
-}, { threshold: 0.2 });
+}, { threshold: 0.1, rootMargin: '0px' });
+document.querySelectorAll('#offer1 .offer2-desc-box').forEach(box => observer2.observe(box));
+});
 
-document.querySelectorAll('#offer1').forEach(box => observer2.observe(box));
+
+
+document.addEventListener('DOMContentLoaded', () => {
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      console.log('JavaScript Works');
+    }
+  });
+}, { threshold: 0.1, rootMargin: '0px' });
+document.querySelectorAll('#offer1 .offer1-desc-box').forEach(box => observer2.observe(box));
+});
+
+
 
 console.log("meow hi im here");
 console.log("cooperation, please... :c");
